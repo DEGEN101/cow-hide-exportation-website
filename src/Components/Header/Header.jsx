5f8d0f3df51f2a495ae3a9210a557810b42e914b
YSR-Kali <yassirali090@gmail.com>
2024-12-28 17:43:49 +0200

    Created Header Content

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import Text from "../Text/Text";
+
+import "./Header.css"
+import logo from "../../Assets/Images/UI/logo.png";
+
+const Header = () => {
+
+    return (
+    <>
+        <section className="header-container">
+
+            <section className="header-left">
+                <img src={logo} alt="Logo" className="header-logo" />
+                <h1 className="header-title">Cowhide Exportation Ltd</h1>
+            </section>
+
+        
+            <nav className="header-nav">
+                <a href="#home" className="nav-link">Home</a>
+                <a href="#gallery" className="nav-link">Gallery</a>
+                <a href="#about" className="nav-link">About</a>
+                <a href="#contact" className="nav-link">Contact</a>
+            </nav>
+        </section>
+    
+    
+    </>
+
+
+    )
+
+
+
+
+
+}
+
+export default Header;
