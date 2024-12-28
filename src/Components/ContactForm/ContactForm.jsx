eaae084b32391a300a5e7c6f339a0abbef7cf8f8
YSR-Kali <yassirali090@gmail.com>
2024-12-28 19:57:29 +0200

    Uploaded Contact Form Icons Updated Contact Form Design

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import './ContactForm.css';
+import clockIcon from "../../Assets/Images/UI/Icons/clock_icon.png";
+import locationIcon from "../../Assets/Images/UI/Icons/location_icon.png";
+import phoneIcon from "../../Assets/Images/UI/Icons/phone_icon.png";
 
 const ContactForm = () => {
   return (
     <div className="contact-form-container">
       <div className="contact-info">
         <div className="contact-item">
-          <span className="icon"></span>
+          <span className="icon"> <img src={locationIcon}/></span>
           <div>
             <h4>Address</h4>
             <p>224 Rua Dos Irmaos Roby,<br />Maputo 1110, Mozambique</p>
           </div>
         </div>
         <div className="contact-item">
-          <span className="icon"></span>
+          <span className="icon"> <img src={phoneIcon}/></span>
           <div>
             <h4>Phone</h4>
             <p>Mobile: +258 85 879 9377<br />Hotline: +258 86 069 5853</p>
           </div>
         </div>
         <div className="contact-item">
-          <span className="icon"></span>
+          <span className="icon"> <img src={clockIcon}/></span>
           <div>
             <h4>Working Time</h4>
             <p>Monday-Friday: 9:00 - 22:00<br />Saturday-Sunday: 9:00 - 21:00</p>
