34f3bf494462276210c4cf642d23be2b4f46470a
YSR-Kali <yassirali090@gmail.com>
2024-12-28 19:03:03 +0200

    Created Contact Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Homepage from './Pages/Homepage/Homepage';
 // import AboutUsPage from './Pages/AboutUsPage/AboutUsPage';
+import ContactForm from './Components/ContactForm/ContactForm';
 
 const App = () => {
   return (
     <>
-      <Homepage/>
+      <ContactForm/>
     </>
   );
 }
---
+++ src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './ContactForm.css';
+
+const ContactForm = () => {
+  return (
+    <div className="contact-form-container">
+      <div className="contact-info">
+        <div className="contact-item">
+          <span className="icon"></span>
+          <div>
+            <h4>Address</h4>
+            <p>224 Rua Dos Irmaos Roby,<br />Maputo 1110, Mozambique</p>
+          </div>
+        </div>
+        <div className="contact-item">
+          <span className="icon"></span>
+          <div>
+            <h4>Phone</h4>
+            <p>Mobile: +258 85 879 9377<br />Hotline: +258 86 069 5853</p>
+          </div>
+        </div>
+        <div className="contact-item">
+          <span className="icon"></span>
+          <div>
+            <h4>Working Time</h4>
+            <p>Monday-Friday: 9:00 - 22:00<br />Saturday-Sunday: 9:00 - 21:00</p>
+          </div>
+        </div>
+      </div>
+      <form className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Your name</label>
+          <input type="text" id="name" placeholder="Abc" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="email" id="email" placeholder="Abc@def.com" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="subject">Subject</label>
+          <input type="text" id="subject" placeholder="This is an optional" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea id="message" rows="4" placeholder="Hi! I’d like to ask about"></textarea>
+        </div>
+        <button type="submit" className="submit-button">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
