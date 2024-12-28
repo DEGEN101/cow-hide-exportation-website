bbf2f1f1c26814d971b376505ddf6232a6df487a
YSR-Kali <yassirali090@gmail.com>
2024-12-28 20:01:59 +0200

    Added img alt text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Homepage from './Pages/Homepage/Homepage';
 // import AboutUsPage from './Pages/AboutUsPage/AboutUsPage';
-import ContactForm from './Components/ContactForm/ContactForm';
+// import ContactForm from './Components/ContactForm/ContactForm';
 
 const App = () => {
   return (
     <>
-      <ContactForm/>
+      <Homepage/>
     </>
   );
 }
--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -9,21 +9,21 @@ const ContactForm = () => {
     <div className="contact-form-container">
       <div className="contact-info">
         <div className="contact-item">
-          <span className="icon"> <img src={locationIcon}/></span>
+          <span className="icon"> <img src={locationIcon} alt="Location icon" /></span>
           <div>
             <h4>Address</h4>
             <p>224 Rua Dos Irmaos Roby,<br />Maputo 1110, Mozambique</p>
           </div>
         </div>
         <div className="contact-item">
-          <span className="icon"> <img src={phoneIcon}/></span>
+          <span className="icon"> <img src={phoneIcon} alt="phone icon" /></span>
           <div>
             <h4>Phone</h4>
             <p>Mobile: +258 85 879 9377<br />Hotline: +258 86 069 5853</p>
           </div>
         </div>
         <div className="contact-item">
-          <span className="icon"> <img src={clockIcon}/></span>
+          <span className="icon"> <img src={clockIcon} alt="clock icon" /></span>
           <div>
             <h4>Working Time</h4>
             <p>Monday-Friday: 9:00 - 22:00<br />Saturday-Sunday: 9:00 - 21:00</p>
