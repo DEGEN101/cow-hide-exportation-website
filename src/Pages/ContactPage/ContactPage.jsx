5ee9de2709ead35031bcad57dba90750a5bc6774
YSR-Kali <yassirali090@gmail.com>
2024-12-28 20:23:24 +0200

    Created Contact Page

---
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,35 @@
+import TitleCard from "../../Components/TitleCard/TitleCard";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import Text from "../../Components/Text/Text";
+import Subheading from "../../Components/Subheading/Subheading";
+import ContactForm from "../../Components/ContactForm/ContactForm"
+import BrandPanel from "../../Components/BrandPanel/BrandPanel";
+import "./ContactPage.css";
+
+
+
+const ContactPage = () => {
+    return (
+    <>
+        <Header/>
+        <TitleCard title="Contact Us" subtitle="Contact"/>
+
+        <section className="information-section-container">
+                    <Subheading> Get In Touch With Us </Subheading>
+                    <Text>
+                    For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!
+                    </Text>
+
+                </section>
+
+        <ContactForm/>
+
+        <BrandPanel/>
+        
+        <Footer/>
+    </>
+    );
+}
+
+export default ContactPage;
