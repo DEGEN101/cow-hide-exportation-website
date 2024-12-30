411d5fa951bf313aef411bb6848a7bf99083eda3
DEGEN101 <ntando.raji13@gmail.com>
2024-12-30 14:59:26 +0200

    Attempt #3 to fix 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import { HashRouter as Router, Route, Routes} from 'react-router-dom'; 
 
 import Homepage from './Pages/Homepage/Homepage';
 import AboutUsPage from './Pages/AboutUsPage/AboutUsPage';
@@ -7,7 +7,7 @@ import AboutUsPage from './Pages/AboutUsPage/AboutUsPage';
 
 const App = () => {
     return (
-        <Router basename="/cow-hide-exportation-website">
+        <Router hashType="slash">
             <Routes>
                 <Route path="/" exact element={<Homepage/> }/>
                 <Route path="/home" element={<Homepage />} />
