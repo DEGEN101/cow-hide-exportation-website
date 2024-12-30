c576819f5f2fe4d93f11b5cb36dd36b5d41175f4
DEGEN101 <ntando.raji13@gmail.com>
2024-12-30 14:47:08 +0200

    Added routing funtionality to footer links

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-// import Homepage from './Pages/Homepage/Homepage';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom'; 
+
+import Homepage from './Pages/Homepage/Homepage';
 import AboutUsPage from './Pages/AboutUsPage/AboutUsPage';
 // import ContactForm from './Components/ContactForm/ContactForm';
-//  import ContactPage from './Pages/ContactPage/ContactPage';
+ import ContactPage from './Pages/ContactPage/ContactPage';
 
 const App = () => {
-  return (
-    <>
-      <AboutUsPage/>
-    </>
-  );
+    return (
+        <Router hashType="slash">
+            <Routes>
+                <Route path="/" exact element={<Homepage/> }/>
+                <Route path="/home" element={<Homepage />} />
+                <Route path="/about" element={<AboutUsPage />}/>
+                <Route path="/contact" element={<ContactPage />}/>
+            </Routes>
+      </Router>
+    );
 }
 
 export default App;
