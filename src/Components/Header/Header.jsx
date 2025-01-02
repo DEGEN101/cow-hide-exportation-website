07bb533b1002aa1de3d39d28a65001abe83575f3
DEGEN101 <ntando.raji13@gmail.com>
2025-01-02 20:11:01 +0200

    Made Header Component more responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 import "./Header.css"
+
 import logo from "../../Assets/Images/UI/Icons/OG_Icons/logo.png";
+import burgerIcon from "../../Assets/Images/UI/Icons/burger-menu-svgrepo-com.svg"
+import closeIcon from "../../Assets/Images/UI/Icons/close-svgrepo-com.svg"
 
 const Header = () => {
+    const [toggleMobileNavigation, setToggleMobileNavigation] = useState("false");
+    
+    const onMobileNavidationButtonClick = () => {
+        if (toggleMobileNavigation === "false") {
+            setToggleMobileNavigation("true")
+        }
+        else {
+            setToggleMobileNavigation("false");
+        }
+
+        console.log(toggleMobileNavigation);
+    }
 
     return (
     <>
@@ -12,12 +29,27 @@ const Header = () => {
                 <h1 className="header-title">Cowhide Exportation Ltd</h1>
             </section>
 
-        
-            <nav className="header-nav">
-                <a href="#home" className="nav-link">Home</a>
-                <a href="#gallery" className="nav-link">Gallery</a>
-                <a href="#about" className="nav-link">About</a>
-                <a href="#contact" className="nav-link">Contact</a>
+            <button  
+                onClick={() => onMobileNavidationButtonClick()} aria-controls="header-nav" 
+                className="modile-toggle-nav" aria-expanded={toggleMobileNavigation}> 
+                <img src={toggleMobileNavigation === "false" ? burgerIcon : closeIcon} alt="navigation burger icon"/>
+            </button>
+
+            <nav>
+                <ul id="header-nav" className="header-nav" data-visible={toggleMobileNavigation}>
+                    <li>
+                        <a href="#/home" className="nav-link">Home</a>
+                    </li>
+                    <li>
+                        <a href="#/gallery" className="nav-link">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="#/about" className="nav-link">About</a>
+                    </li>
+                    <li>
+                        <a href="#/contact" className="nav-link">Contact</a>
+                    </li>
+                </ul>
             </nav>
         </section>
     
